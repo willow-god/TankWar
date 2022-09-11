62a15d9b98b15009610baecfbc9984989cce18ee
WollowGod <3162475700@qq.com>
2022-09-11 14:02:19 +0800

    编写了游戏父类

--- src/tankwar/GamePanel.java
+++ src/tankwar/GamePanel.java
@@ -7,10 +7,12 @@ import java.awt.event.KeyEvent;
 
 public class GamePanel extends JFrame {
 
+
+    Image offScreenImage = null;//定义双缓存图片
     //游戏状态 0未开始 1运行中 2暂停 3失败 4成功
     public int state = 0;
     private boolean start = false;//游戏是否开始
-    int a = 0;//临时变量
+    int a = 1;//临时变量
 
     int width = 800;
     int height = 610;
@@ -46,30 +48,38 @@ public class GamePanel extends JFrame {
 
     @Override
     public  void paint(Graphics g) {
+        //创建和容器一样大小的图片
+        if (offScreenImage == null) {
+            offScreenImage = this.createImage(width,height);
+        }
+        //获得此图片相应的画布
+        Graphics gImage = offScreenImage.getGraphics();
         //设置背景颜色
-        g.setColor(Color.gray);
+        gImage.setColor(Color.gray);
         //填充整个屏幕
-        g.fillRect(0,0,width,height);
+        gImage.fillRect(0,0,width,height);
         //改变画笔颜色
-        g.setColor(Color.blue);
+        gImage.setColor(Color.blue);
         //改变文字大小和样式
-        g.setFont(new Font("仿宋",Font.BOLD,50));
+        gImage.setFont(new Font("仿宋",Font.BOLD,50));
         if (state == 0) {
-            g.drawString("选择游戏模式",220,100);
-            g.drawString("单人模式",220,200);
-            g.drawString("双人模式",220,300);
-            g.drawString("按1，2选择游戏，按回车开始游戏",0,400);
-            g.drawImage(select,160,y,null);
+            gImage.drawString("选择游戏模式",220,100);
+            gImage.drawString("单人模式",220,200);
+            gImage.drawString("双人模式",220,300);
+            gImage.drawString("按1，2选择游戏，按回车开始游戏",0,400);
+            gImage.drawImage(select,160,y,null);
         }
         else if (state ==1||state == 2) {
-            g.drawString("游戏开始",220,300);
+            gImage.drawString("游戏开始",220,300);
             if(state == 1){
-                g.drawString("单人模式",220,200);
+                gImage.drawString("单人模式",220,200);
             }
             else{
-                g.drawString("双人模式",220,200);
+                gImage.drawString("双人模式",220,200);
             }
         }
+        //将缓冲区绘制好的图形绘制到容器中
+        g.drawImage(offScreenImage,0,0,null);
     }
 
 
@@ -83,19 +93,19 @@ public class GamePanel extends JFrame {
                         y = 150;
                         a = 1;
                     }
-                    System.out.println(state);
+                    //System.out.println(state);//调试使用
                     break;
                 case KeyEvent.VK_2:
                     if (!start) {
                         y=250;
                         a=2;
                     }
-                    System.out.println(state);
+                    //System.out.println(state);//调试使用
                     break;
                 case KeyEvent.VK_ENTER:
                     state = a;
                     start = true;
-                    System.out.println("state:"+""+state);
+                    //System.out.println("state:"+""+state);//调试使用
                     break;
                 default:
                     break;
